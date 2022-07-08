1fb1e9c9697948949ef879dc423f66af0a7924c9
Nathan Dryden <nathan.dryden@vanderbilt.edu>
2022-07-08 15:21:08 -0500

    Test commit of dummy table

---
+++ dbt/models/example/table_creation_test.sql
@@ -0,0 +1,16 @@
+/*A test table creation with stg data*/
+{{ config(materialized='table') }}
+
+with contacts_test_tbl as (
+select acct.account_id
+, acct.masterrecordid
+, acct.name
+, acct.phone
+, acct.accountnumber
+, acct.AQB__NEXTACTIVITYDATE__C
+,cnct.contact_ID
+from DRYDENNE_STAGE.STG_SFDC_AQB__ACCOUNT acct
+	join DRYDENNE_STAGE.STG_SFDC_AQB__CONTACT CNCT 
+		on acct.ACCOUNT_ID = cnct.accountID
+where acct.ISDELETED = 0 and cnct.isdeleted = 0
+)
