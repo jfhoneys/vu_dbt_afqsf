1c166ee0dc148fb342a690fa4107ece8422931f4
Nathan Dryden <nathan.dryden@vanderbilt.edu>
2022-07-14 11:23:05 -0500

    Draft of education_c

--- dbt/models/staging/integration/int_sfdc_aqb_education_c.sql
+++ dbt/models/staging/integration/int_sfdc_aqb_education_c.sql
@@ -1,11 +1,9 @@
 with all_columns as ( /*base data set*/
-    select 
-    *
-    --count(*) /*record count 416026*/
+    select *
+--   count(*) /*record count 416026*/
     from stg_sfdc_aqb__aqb__education__c
     where not isdeleted
-    )
-
+)
    , owner as ( /*record owner user info*/
     select user_id as owner_userid
          , username as owner_username
@@ -51,51 +49,53 @@ with all_columns as ( /*base data set*/
    , edu as (
     select all_columns.aqb__education__c_id as education_key
          , owner.owner_userid
+--         , isdeleted
          , owner.owner_name
-         , all_columns.recordtypeid
-         , all_columns.createddate created_date
+         , "NAME" /*<<field identification need*/
+         , recordtypeid
+         , createddate
          , created_by.creator_userid
          , created_by.creator_name
          , all_columns.lastmodifieddate last_modified_date
          , last_update_by.lastupd_userid
          , last_update_by.lastupd_name
-         , systemmodstamp
-         , lastvieweddate
-         , lastreferenceddate
-         , aqb__areasofstudy__c
-         , aqb__campus__c
-         , aqb__certificationdescription__c
-         , aqb__certification__c
-         , aqb__conferringentity__c
-         , aqb__contactid__c
-         , aqb__dateissued__c
-         , aqb__degreediploma__c
-         , aqb__degreelevel__c
-         , aqb__department__c
-         , aqb__entryclassyear__c
-         , aqb__expirationdate__c
-         , aqb__firstyearofattendance__c
-         , aqb__graduationyear__c
-         , aqb__graduation_month__c
-         , aqb__granting_school__c
-         , aqb__honorarydegree__c
-         , aqb__institution__c
-         , aqb__institutionalunit__c
-         , aqb__isprimarydegree__c
-         , aqb__lastyearofattendance__c
-         , aqb__ownerunit__c
-         , aqb__preferredyear__c
-         , aqb__school__c
-         , aqb__source__c
-         , aqb__specialty__c
-         , aqb__status__c
-         , aqb__subspecialtycategory__c
-         , aqb__subspecialty__c
-         , aqb__type__c
-         , aqb__studentstatus__c
-         , aqcv_conversionid__c
-         , vu_gold_start_year__c
-         , vu_gold_end_year__c
+--         , all_columns.systemmodstamp
+--         , all_columns.lastvieweddate
+--         , all_columns.lastreferenceddate
+         , aqb__areasofstudy__c as area_of_study
+--    , aqb__campus__c
+--    , aqb__certificationdescription__c
+--    , aqb__certification__c
+--    , aqb__conferringentity__c
+         , aqb__contactid__c as contact_id_fk
+         , aqb__dateissued__c as degree_date
+         , aqb__degreediploma__c as degree_diploma
+         , aqb__degreelevel__c as degree_level
+--         , aqb__department__c
+--         , aqb__entryclassyear__c
+--         , aqb__expirationdate__c
+--         , aqb__firstyearofattendance__c
+         , aqb__graduationyear__c as graduation_year
+         , aqb__graduation_month__c as graduation_month
+         , aqb__granting_school__c as granting_school
+         , aqb__honorarydegree__c as honerary_degree
+         , aqb__institution__c as institution
+--         , aqb__institutionalunit__c
+         , aqb__isprimarydegree__c as is_primary_degree
+--         , aqb__lastyearofattendance__c
+--         , aqb__ownerunit__c
+         , aqb__preferredyear__c as preferred_year
+         , aqb__school__c as school
+--         , aqb__source__c
+--         , aqb__specialty__c
+--         , aqb__status__c
+--         , aqb__subspecialtycategory__c
+--         , aqb__subspecialty__c
+--         , aqb__type__c
+--         , aqb__studentstatus__c
+--         , aqcv_conversionid__c
+--         , vu_gold_start_year__c
+--         , vu_gold_end_year__c
          , matillion_batch_id
          , matillion_updated_timestamp
          , source_name
@@ -105,5 +105,6 @@ with all_columns as ( /*base data set*/
              join last_update_by on all_columns.ownerid = last_update_by.lastupd_userid
 )
 
+/*clean select*/
 select *
 from edu
