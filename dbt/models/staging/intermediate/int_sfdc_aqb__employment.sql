87dc0beac61f5f97649af06497b19df3194efd49
Poris, Jillian <jillian.poris@vanderbilt.edu>
2022-07-26 08:48:18 -0500

    Added underscores

--- dbt/models/staging/intermediate/int_sfdc_aqb__employment.sql
+++ dbt/models/staging/intermediate/int_sfdc_aqb__employment.sql
@@ -6,9 +6,9 @@ last_modified_by as (select * from {{ref ('stg_sfdc_aqb__user')}}),
 employment_transform as (
     select employment.id as employment_id
 -- , employment.ownerid as employment_ownerid
-, employment.isdeleted as employment_isdeleted
+, employment.isdeleted as employment_is_deleted
 , employment.name as employment_name
-, employment.recordtypeid as employment_recordtypeid
+, employment.recordtypeid as employment_record_type_id
 -- , employment.createddate as employment_createddate
 -- , employment.createdbyid as employment_createdbyid
 -- , employment.lastmodifieddate as employment_lastmodifieddate
@@ -17,28 +17,28 @@ employment_transform as (
 -- , employment.aqb__assistant_s_email__c as employment_ssistant_s_email
 -- , employment.aqb__assistant_s_name__c as employment_ssistant_s_name
 -- , employment.aqb__assistant_s_phone__c as employment_ssistant_s_phone
-, employment.aqb__businessphone__c as employment_usinessphone
-, employment.aqb__contactid__c as employment_contactid
+, employment.aqb__businessphone__c as employment_usiness_phone
+, employment.aqb__contactid__c as employment_contact_id
 -- , employment.aqb__corporate_phone__c as employment_corporate_phone
 , employment.aqb__donotarchive__c as employment_donotarchive
 -- , employment.aqb__earnings__c as employment_earnings
-, employment.aqb__employernamedisplay__c as employment_employernamedisplay
+, employment.aqb__employernamedisplay__c as employment_employer_name_display
 -- , employment.aqb__employername__c as employment_employername
-, employment.aqb__employmentcomment__c as employment_employmentcomment
-, employment.aqb__employmentexternalid__c as employment_employmentexternalid
-, employment.aqb__enddate__c as employment_enddate
+, employment.aqb__employmentcomment__c as employment_employment_comment
+, employment.aqb__employmentexternalid__c as employment_employment_external_id
+, employment.aqb__enddate__c as employment_end_date
 , employment.aqb__ended__c as employment_ended
 -- , employment.aqb__fax_number__c as employment_fax_number
 -- , employment.aqb__industry__c as employment_industry
-, employment.aqb__isprimaryemployer__c as employment_isprimaryemployer
+, employment.aqb__isprimaryemployer__c as employment_is_primary_employer
 , employment.aqb__level__c as employment_level
 -- , employment.aqb__linkedaddress__c as employment_linkedaddress
-, employment.aqb__linkedemployer__c as employment_linkedemployer
+, employment.aqb__linkedemployer__c as employment_linked_employer
 , employment.aqb__occupation__c as employment_occupation
 -- , employment.aqb__ownerunit__c as employment_ownerunit
-, employment.aqb__primaryemployment__c as employment_primaryemployment
+, employment.aqb__primaryemployment__c as employment_primary_employment
 -- , employment.aqb__source__c as employment_source
-, employment.aqb__startdate__c as employment_startdate
+, employment.aqb__startdate__c as employment_start_date
 -- , employment.aqb__started__c as employment_started
 , employment.aqb__status__c as employment_status
 , employment.aqb__title__c as employment_title
